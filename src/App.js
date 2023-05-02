9356711a427fc47845721fb1fcd348470d50e55c
Dinesh Yadav <dinesh@getdefault.in>
2023-05-02 17:19:02 +0530

    fix : files and folder structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import Router from "./routing/Router";
+
+const App = () => {
+  return (
+    <>
+      <Router />
+    </>
+  );
+};
+
+export default App;
